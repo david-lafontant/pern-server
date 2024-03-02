e29cedef76ab2d6227191e993417ea28ad194357
David Lafontant <lafontant.david@gmail.com>
2024-03-01 20:37:47 -0500

    Get all todos

--- index.js
+++ index.js
@@ -1,25 +1,20 @@
 const express = require('express');
 const cors = require('cors');
 const pool = require('./db');
-
+const port = 5000;
 // create app
-
 const app = express();
 
-
 // middleware
-
 app.use(cors());
 app.use(express.json());
 
-
 // create a todo - POST
-
 app.post("/todos", async (req, res) => {
   try {
     const { content } = req.body;
-    const newTodo = await pool.query("INSERT INTO todo (content) VALUES($1)", [content]);
-    res.json(newTodo);
+    const newTodo = await pool.query("INSERT INTO todo (content) VALUES($1) RETURNING * ", [content]);
+    res.json(newTodo.rows);
   } catch (err) {
 
     console.error(err.message);
@@ -27,9 +22,19 @@ app.post("/todos", async (req, res) => {
 
 });
 
+// get all todos
+app.get('/todos', async (req, res) => {
+  try {
+    const todos = await pool.query("SELECT * FROM todo");
+    res.json(todos.rows);
+  } catch (err) {
+    console.error(err.message)
+  }
+});
+
 
 //launch server
 
-app.listen(5000, () => {
+app.listen(port, () => {
   console.log("Api is running and running! ad");
 });
