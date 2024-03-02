dd98b7287ba5d7819206e0884800ef381333c454
David Lafontant <lafontant.david@gmail.com>
2024-03-01 19:00:18 -0500

    Create post routes

--- index.js
+++ index.js
@@ -1,14 +1,35 @@
 const express = require('express');
 const cors = require('cors');
+const pool = require('./db');
 
 // create app
+
 const app = express();
+
+
 // middleware
+
 app.use(cors());
 app.use(express.json());
 
 
+// create a todo - POST
+
+app.post("/todos", async (req, res) => {
+  try {
+    const { content } = req.body;
+    const newTodo = await pool.query("INSERT INTO todo (content) VALUES($1)", [content]);
+    res.json(newTodo);
+  } catch (err) {
+
+    console.error(err.message);
+  }
+
+});
+
+
 //launch server
+
 app.listen(5000, () => {
   console.log("Api is running and running! ad");
 });
