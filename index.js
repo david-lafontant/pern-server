b457c3080950b6926f1dd8247809a274a19ed22a
David Lafontant <lafontant.david@gmail.com>
2024-03-01 20:38:30 -0500

    Get a specific todo

--- index.js
+++ index.js
@@ -32,6 +32,14 @@ app.get('/todos', async (req, res) => {
   }
 });
 
+// get a specific todo 
+app.get('/todos/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const todo = await pool.query("SELECT * FROM todo WHERE todo_id = ($1)", [id]);
+    res.json(todo.rows[0]);
+  } catch (err) { console.error(err.message); }
+})
 
 //launch server
 
